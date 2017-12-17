ee6249dfd96a9c7fd8bad9112846b680ab443657
Jeremy Press <jpress@box.com>
2017-12-17 10:28:42 -0800

    styles

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -61,24 +61,26 @@ export default class ExpenseForm extends React.Component {
 
     render() {
         return (
-            <div>
-                {this.state.error && <p>{this.state.error}</p>}
-                <form
-                    onSubmit={this.onSubmit}
-                    >
-                    <input
-                        type="text"
-                        placeholder="Description"
-                        autoFocus
-                        value={this.state.description}
-                        onChange={this.onDescriptionChange}
-                    />
-                    <input
-                        type="text"
-                        placeholder="Amount"
-                        value={this.state.amount}
-                        onChange={this.onAmountChange}
-                    />
+            <form
+                className="form"
+                onSubmit={this.onSubmit}
+                >
+                {this.state.error && <p className="form__error">{this.state.error}</p>}
+                <input
+                    type="text"
+                    className="text-input"
+                    placeholder="Description"
+                    autoFocus
+                    value={this.state.description}
+                    onChange={this.onDescriptionChange}
+                />
+                <input
+                    type="text"
+                    className="text-input"
+                    placeholder="Amount"
+                    value={this.state.amount}
+                    onChange={this.onAmountChange}
+                />
                 <SingleDatePicker
                     date={this.state.createdAt}
                     onDateChange={this.onDateChange}
@@ -89,14 +91,16 @@ export default class ExpenseForm extends React.Component {
 
                 />
                 <textarea
+                    className="select-area"
                     placeholder="add a note for your expense"
                     value={this.state.note}
                     onChange={this.onNoteChange}
                     >
                 </textarea>
-                <button>Add Expense</button>
-                </form>
-            </div>
+                <div>
+                    <button className="btn">Save Expense</button>
+                </div>
+            </form>
         )
     }
 }
--- src/components/ExpenseSummary.js
+++ src/components/ExpenseSummary.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import numeral from 'numeral';
+import { Link } from 'react-router-dom';
 import ExpenseListItem from './ExpenseListItem'
 import selectVisibleExpenses from '../selectors/expenses';
 import selectExpensesSum from '../selectors/expenses-total';
 
 
 export const ExpenseSummary = ({expensesCount, sum}) => (
-    <div>
-        {expensesCount > 0 &&
-            <h1>
-                Viewing {expensesCount} {expensesCount=== 1 ? 'expense' : 'expenses'} for a total of {numeral(sum / 100).format('$0,0.00')}
-            </h1>
-        }
+    <div className="page-header">
+        <div className="content-container">
+            {expensesCount > 0 &&
+                <h1 className="page-header__title">
+                    Viewing <span>{expensesCount}</span> {expensesCount=== 1 ? 'expense' : 'expenses'} for a total of <span>{numeral(sum / 100).format('$0,0.00')}</span>
+                </h1>
+            }
+        <div className="page-header__action">
+            <Link className="btn" to="/create">Add Expense</Link>
+        </div>
+        </div>
     </div>
 )
 
